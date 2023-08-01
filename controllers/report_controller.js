b08d567472434d7cdf98bbb4fa9ae6a518edbe68
iradecimal <iggesmundo@up.edu.ph>
2023-08-01 13:02:21 +0800

    changed some field names

--- controllers/report_controller.js
+++ controllers/report_controller.js
@@ -7,29 +7,28 @@ exports.newReport = asyncHandler(async (req, res, next) => {
         reptype: req.body.reptype,
         det: req.body.det 
     });
-    try {
-        await newReport.save();
-        res.status(200).send("Success");
-    } catch {
-        res.status(400).send("Report cannot be created");
-    }
+    await newReport.save()
+            .then(() => {
+                res.status(201).json(newReport);
+            })
+            .catch((error) => {
+                res.status(400);
+            });
 });
 
 exports.viewReport = asyncHandler(async (req, res, next) => {
-    const reports = await Report.findById(req.body.oid).select(
+    const reports = await Report.findById(req.params.oid).select(
     'datetime reptype det status').exec();
     if (reports === null) {
-        console.log(err);
         res.status(404).send("Reports cannot be found");
     }
 
-    res.status(200).json(reports);
+    res.json(reports);
 });
 
 exports.findUserReport = asyncHandler(async (req, res, next) => {
     const reports = await Report.find({uid: req.params.uid});
     if (reports === null) {
-        console.log(err);
         res.status(404).send("Reports cannot be found");
     }
     res.status(201).json(reports);
@@ -48,7 +47,6 @@ exports.flagReport = asyncHandler(async (req, res, next) => {
 exports.deleteReport = asyncHandler(async (req, res, next) => {
     const del = await Report.deleteOne(req.params.oid);
     if (del == 0) {
-        console.log(err);
         res.status(404).send("Reports cannot be found");
     }
 
