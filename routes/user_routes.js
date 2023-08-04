c4c1b9c7aade01258be71fe2862745445f2b0a98
iradecimal <iggesmundo@up.edu.ph>
2023-08-04 11:42:28 +0800

    added: prof pic upload and get

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,14 +1,29 @@
 const express = require("express");
 const router = express.Router();
 const UserController = require('../controllers/user_controller.js');
+const multer  = require('multer');
+const path = require("path");
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, '../profpics');
+    },
+    filename: function(req, file, cb){
+        console.log(file);
+        cb(null, req.params.uid +  path.extname(file.originalname));
+    }
+})
+
+const upload = multer({ storage: storage });
 
 router.post("/signup", UserController.signup);
 router.post("/login", UserController.login);
 router.get("/logout", UserController.logout);
-//router.post("/upload-prof-pic", UserController.uploadPicture);
+router.post("/upload-pic/:uid", upload.single("image"), UserController.uploadPicture);
 router.get("/get-user/:uid", UserController.getUser);
 router.get("/get-full-name/:uid", UserController.getFullName);
-router.get("/get-picture/:uid", UserController.getProfilePicture);
+router.get("/get-pic/:uid", UserController.getProfilePicture);
 router.get("/get-age/:uid", UserController.getUserAge);
 router.patch("/update-metrics/:uid", UserController.updateMetrics);
 router.patch("/update-bio/:uid", UserController.updateBio);
