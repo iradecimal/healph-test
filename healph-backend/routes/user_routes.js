294666038fc57388e50d1f361cb8f5be0262baa7
Marick Rei Asilo <60131697+mbasilo@users.noreply.github.com>
2023-10-19 13:19:41 +0800

    chore: fixed mixup between user_controller.js and user_routes.js
    reverted user_routes.js to latest version and updated user_controller.js to match backend.

--- healph-backend/routes/user_routes.js
+++ healph-backend/routes/user_routes.js
@@ -1,167 +1,37 @@
-const asyncHandler = require('express-async-handler');
-const User = require('../models/user.js');
-const path = require("node:path");
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const router = express.Router();
+const UserController = require('../controllers/user_controller.js');
+const multer  = require('multer');
+const path = require("path");
+const Auth = require('../auth/auth_token_handler.js');
 
-const maxAge = 28 * 24 * 60 * 60;
-const createToken = (id) => {
-    return jwt.sign({ id }, 'HealPHScrambler', {
-      expiresIn: maxAge
-    });
-  };
 
-exports.signup = asyncHandler(async (req, res, next) => {
-    const newUser = new User({
-        email: req.body.email,
-        pass: req.body.password,
-        uname: req.body.username,
-        name: {
-            fname: req.body.firstName,
-            lname: req.body.lastName,
-            mi: req.body.middleInitial,
-            suffix: req.body.suffix
-        },
-        sex: req.body.sex,
-        bday: req.body.birthday,
-        loc:{
-            region: req.body.region,
-            town: req.body.town
-        },   
-        uni: req.body.university,
-        deg: req.body.degree,
-        illnesses: req.body.illnesses,
-        allergies: req.body.allergies,
-        weight: req.body.weight,
-        height: req.body.height
-    });
-
-    try{
-        console.log(newUser);
-        await newUser.save();
-        const token = createToken(user._id);
-        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
-        res.status(201).json({uid: newUser._id});
-    } catch(err) {
-        console.log(err)
-        if (err.code === 11000){
-            res.status(400).json({ error: "Username/Email has already been used"});
-        } else {
-            res.status(400).json(err); 
-        }  
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './profpics');
+    },
+    filename: function(req, file, cb){
+        console.log(file);
+        cb(null, req.params.uid +  path.extname(file.originalname));
     }
+})
+
+const upload = multer({ storage: storage });
+
+router.post("/signup", UserController.signup);
+router.post("/login", UserController.login);
+router.get("/logout", Auth.userAuth, UserController.logout);
+router.post("/upload-pic/:uid", Auth.userAuth,  upload.single("image"), function (req, res){
+    console.log(req.file);
+    res.send("Single File upload success");
 });
+router.get("/get-user/:uid", Auth.userAuth, UserController.getUser);
+router.get("/get-full-name/:uid", Auth.userAuth, UserController.getFullName);
+router.get("/get-pic/:uid", Auth.userAuth, UserController.getProfilePicture);
+router.get("/get-age/:uid", Auth.userAuth, UserController.getUserAge);
+router.patch("/update-metrics/:uid", Auth.userAuth, UserController.updateMetrics);
+router.patch("/update-bio/:uid", Auth.userAuth, UserController.updateBio);
+router.patch("/update-password/:uid", Auth.userAuth, UserController.updatePassword);
+//router.get("/confirm-user/:uid", UserController.confirmUser);
 
-exports.login = asyncHandler(async (req, res, next) => {
-    try {
-        const user = await User.login(req.body.email, req.body.password);
-        console.log(user);
-        const token = createToken(user._id);
-        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
-        res.status(200).json({ user: user._id });
-    } catch (err) {
-        res.status(400).json(err);
-    }
-});
-
-exports.logout = asyncHandler(async (req, res, next) => {
-    res.cookie('jwt', '', { maxAge: 1 });
-    res.send("User Logged out.");
-});
-
-exports.getUser = asyncHandler(async (req, res, next) => {
-    const user = await User.findById(req.params.uid).select(
-    'uname name sex bday loc uni deg joindate illnesses allergies weight height').exec();
-
-    if (user === null) {
-        res.status(404).send("User cannot be found");
-    }
-    
-
-    res.status(200).json({
-        uname: user.uname,
-        name: user.name,
-        sex: user.sex,
-        bday: user.bday,
-        loc: user.uname,
-        uni: user.uni,
-        deg: user.deg,
-        joindate: user.joindate,
-        illnesses: user.illnesses,
-        allergies: user.allergies,
-        weight: user.weight,
-        height: user.height
-    });
-});
-
-exports.getFullName = asyncHandler(async (req, res, next) => {
-    const user = await User.findById(req.params.uid).exec();
-
-    if (user === null) {
-        res.status(404).send("User cannot be found");
-    }
-    
-    const query = user.fullName;
-
-    res.status(200).json({fullName: query});
-});
-
-exports.getProfilePicture = asyncHandler(async (req, res, next) => {
-    const user = await User.findById(req.params.uid).exec();
-
-    if (user === null) {
-        res.status(404).send("User cannot be found");
-    }
-    const options = {
-        root: path.join(__dirname, '../profpics')
-    };
-    console.log('/profpics/' + req.params.uid + ".jpg");
-    res.status(200).sendFile( "1" + ".jpg", options);
-});
-
-exports.getUserAge = asyncHandler(async (req, res, next) => {
-    const user = await User.findById(req.params.uid).exec();
-
-    if (user === null) {
-        res.status(404).send("User cannot be found");
-    }
-
-    const query = user.age;
-
-    res.status(200).json({age: query});
-});
-
-exports.updateMetrics = asyncHandler(async (req, res, next) => {
-    await User.findByIdAndUpdate(req.params.uid, {
-        $set: {
-          height: req.body.height,
-          weight: req.body.weight
-        }
-      },);
-    res.status(200).send("Success");
-});
-
-exports.updateBio = asyncHandler(async (req, res, next) => {
-    const user = await User.findByIdAndUpdate(req.params.uid, {
-        $set: {
-            fname: req.body.firstName,
-            lname: req.body.lastName,
-            mi: req.body.middleInitial,
-            suffix: req.body.suffix,
-            sex: req.body.sex,
-            location: req.body.location,
-            uni: req.body.uni,
-            degree: req.body.degree
-        }
-    }, {new: true} );
-    res.status(200).send("Success");
-});
-
-exports.uploadPicture = asyncHandler(async (req, res, next) => {
-    res.status(200).send("Success");
-});
-
-exports.updatePassword = asyncHandler(async (req, res, next) => {
-
-    await User.findByIdAndUpdate(req.params.uid, {password: req.body.password});
-    res.status(200).send("Success");
-});
+module.exports = router;
